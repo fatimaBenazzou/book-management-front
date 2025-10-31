02d6dda5360d7f6287095cf9eff40e70bd235135
fatimaBenazzou <b.fati2102@gmail.com>
2025-10-31 14:33:23 +0100

    set up

---
+++ src/api/config.ts
@@ -0,0 +1,12 @@
+import axios from "axios";
+
+const axiosConfig = axios.create({
+  baseURL: import.meta.env.VITE_API_URL,
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  withCredentials: true,
+});
+
+export default axiosConfig;
